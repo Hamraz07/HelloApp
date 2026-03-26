42cc0bfc329b9d6d74bf93a2f51f7641e8ac982c
Hamraz07 <hamrazhamza07@gmail.com>
2026-03-26 13:44:20 +0530

    Display Hello with Command-Line Argument

--- app.java
+++ app.java
@@ -1,5 +1,6 @@
 public class HelloApp {
-    public static void main(string[] args) {
-	system.out.println(x: "Hello, World!");
+	public static void main(String[] args) {
+		String name = args[0];
+		System.out.println("Hello, "+ name + "!");
 	}
 }
